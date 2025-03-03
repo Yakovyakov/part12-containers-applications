571e2ca2d8f6fc0e37869961f42daf71ed30d987
Yakovyakov <yyak0423@gmail.com>
2025-03-03 12:38:47 -0500

    Update Footer.jsx

--- my-app/frontend/src/components/Footer.jsx
+++ my-app/frontend/src/components/Footer.jsx
@@ -7,7 +7,10 @@ const Footer = () => {
   return (
     <div style={footerStyle}>
       <br />
-      <em>Library app, Full Stack open(GraphQL), course 2024-2025</em>
+      <em>
+        Library app, Full Stack open(GraphQL), course 2024-2025 <br/>
+        Part 12 - Full Stack Web Development: Containers
+      </em>
     </div>
   )
 }
